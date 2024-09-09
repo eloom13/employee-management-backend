7b9244a28d42af4843143bcd3de9f9710aaab441
Elmir Mujkić <116904225+eloom13@users.noreply.github.com>
2024-09-09 12:17:00 +0200

    conf env

--- src/main/java/com/example/springbootcrudapi/SpringBootCrudApiApplication.java
+++ src/main/java/com/example/springbootcrudapi/SpringBootCrudApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootCrudApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootCrudApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCrudApiApplication.class, args);
+    }
 
 }
