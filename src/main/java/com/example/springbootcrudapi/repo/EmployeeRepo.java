c2a88f83de65ee3963ad1456c76eec756478ae56
Elmir Mujkić <116904225+eloom13@users.noreply.github.com>
2024-09-09 13:18:58 +0200

    service class

---
+++ src/main/java/com/example/springbootcrudapi/repo/EmployeeRepo.java
@@ -0,0 +1,12 @@
+package com.example.springbootcrudapi.repo;
+
+import com.example.springbootcrudapi.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+
+    Optional findEmployeeById(Long id);
+}
